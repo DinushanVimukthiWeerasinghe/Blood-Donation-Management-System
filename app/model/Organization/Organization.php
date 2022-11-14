104a1bf2edf6b2be637c05930b60f98a41269203
janith2021 <janithheshara1998@gmail.com>
2022-11-14 15:43:32 +0530

    Create Organization.php

---
+++ app/model/Organization/Organization.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\model\Organization;
+
+class Organization extends \App\model\database\dbModel
+{
+
+    public function rules(): array
+    {
+        // TODO: Implement rules() method.
+    }
+
+    public static function getTableShort(): string
+    {
+        // TODO: Implement getTableShort() method.
+    }
+
+    public static function tableName(): string
+    {
+        return 'campaign';
+    }
+
+    public static function PrimaryKey(): string
+    {
+        // TODO: Implement PrimaryKey() method.
+    }
+
+    public function attributes(): array
+    {
+        // TODO: Implement attributes() method.
+    }
+}
