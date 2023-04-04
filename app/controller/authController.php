ff4df20baf2402a603b2e67c6abd4b13bf87549e
Dinushan <2020cs202@stu.ucsc.cmb.ac.lk>
2023-04-05 00:46:00 +0530

    Added Middlewares

--- app/controller/authController.php
+++ app/controller/authController.php
@@ -2,59 +2,14 @@
 
 namespace App\controller;
 
-use App\model\Authentication\Login;
 use Core\Application;
-use Core\BaseMiddleware;
-use Core\Controller;
-use Core\middleware\AuthenticationMiddleware;
-use Core\Request;
-use Core\Response;
 
-class authController extends Controller
+class AuthController extends \Core\Controller
 {
-
-    public function __construct()
-    {
-        if (Application::$app->getUser()) {
-            $role = Application::$app->getUser()->getRole();
-            Application::Redirect('/' . $role . '/dashboard');
-
-        }else{
-            $this->registerMiddleware(new AuthenticationMiddleware(['managerLogin'], BaseMiddleware::ALLOWED_ROUTES));
-        }
-
-    }
-
-
-
     public function logout()
     {
-        if (Application::$app->isGuest())
-        {
-//            Application::$app->response->redirect('/');
-                Application::$app->response->redirect('/manager/login');
-        }else{
-            Application::$app->logout();
-//            Application::$app->response->redirect('/');
-            Application::$app->response->redirect('/manager/login');
-        }
-    }
-    public function managerLogin(Request $request, Response $response): string
-    {
-        $login = new Login();
-        if ($request->isPost())
-        {
-            $login->loadData($request->getBody());
-            $pass = password_hash($_POST['password'], PASSWORD_DEFAULT);
-
-            if ($login->validate() && $login->login())
-            {
-                $response->redirect('/manager/dashboard');
-                return '';
-            }
-        }
-        $this->layout='NonAuth';
-        return $this->render('Manager/login',['model'=>$login]);
+        Application::$app->session->remove('user');
+        Application::$app->response->redirect('/');
     }
 
 }
